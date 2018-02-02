f5f2ca771c3998dd1e63cd42c5695a2623cdf69a
Holly King <KIN40026556@serc.ac.uk>
2018-02-02 09:39:50 +0000

    Added labels

---
+++ student grades 2/student grades 2/Form1.Designer.cs
@@ -0,0 +1,208 @@
+﻿namespace student_grades_2
+{
+    partial class frmStudentGrades
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblStudentGrades = new System.Windows.Forms.Label();
+            this.lblEnterDetails = new System.Windows.Forms.Label();
+            this.lblStudentName = new System.Windows.Forms.Label();
+            this.lblStudentNumber = new System.Windows.Forms.Label();
+            this.lblGroup = new System.Windows.Forms.Label();
+            this.lblStudentMarks = new System.Windows.Forms.Label();
+            this.lblEnglish = new System.Windows.Forms.Label();
+            this.lblMaths = new System.Windows.Forms.Label();
+            this.lblScience = new System.Windows.Forms.Label();
+            this.gbxResults = new System.Windows.Forms.GroupBox();
+            this.lblStudentDetails = new System.Windows.Forms.Label();
+            this.lblAverageMark = new System.Windows.Forms.Label();
+            this.lblStudentGrade = new System.Windows.Forms.Label();
+            this.gbxResults.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblStudentGrades
+            // 
+            this.lblStudentGrades.AutoSize = true;
+            this.lblStudentGrades.Location = new System.Drawing.Point(217, 26);
+            this.lblStudentGrades.Name = "lblStudentGrades";
+            this.lblStudentGrades.Size = new System.Drawing.Size(81, 13);
+            this.lblStudentGrades.TabIndex = 0;
+            this.lblStudentGrades.Text = "Student Grades";
+            // 
+            // lblEnterDetails
+            // 
+            this.lblEnterDetails.AutoSize = true;
+            this.lblEnterDetails.Location = new System.Drawing.Point(25, 56);
+            this.lblEnterDetails.Name = "lblEnterDetails";
+            this.lblEnterDetails.Size = new System.Drawing.Size(155, 13);
+            this.lblEnterDetails.TabIndex = 1;
+            this.lblEnterDetails.Text = "Please enter the student details";
+            // 
+            // lblStudentName
+            // 
+            this.lblStudentName.AutoSize = true;
+            this.lblStudentName.Location = new System.Drawing.Point(48, 80);
+            this.lblStudentName.Name = "lblStudentName";
+            this.lblStudentName.Size = new System.Drawing.Size(75, 13);
+            this.lblStudentName.TabIndex = 2;
+            this.lblStudentName.Text = "Student Name";
+            // 
+            // lblStudentNumber
+            // 
+            this.lblStudentNumber.AutoSize = true;
+            this.lblStudentNumber.Location = new System.Drawing.Point(48, 104);
+            this.lblStudentNumber.Name = "lblStudentNumber";
+            this.lblStudentNumber.Size = new System.Drawing.Size(84, 13);
+            this.lblStudentNumber.TabIndex = 3;
+            this.lblStudentNumber.Text = "Student Number";
+            // 
+            // lblGroup
+            // 
+            this.lblGroup.AutoSize = true;
+            this.lblGroup.Location = new System.Drawing.Point(48, 129);
+            this.lblGroup.Name = "lblGroup";
+            this.lblGroup.Size = new System.Drawing.Size(36, 13);
+            this.lblGroup.TabIndex = 4;
+            this.lblGroup.Text = "Group";
+            // 
+            // lblStudentMarks
+            // 
+            this.lblStudentMarks.AutoSize = true;
+            this.lblStudentMarks.Location = new System.Drawing.Point(28, 155);
+            this.lblStudentMarks.Name = "lblStudentMarks";
+            this.lblStudentMarks.Size = new System.Drawing.Size(153, 13);
+            this.lblStudentMarks.TabIndex = 5;
+            this.lblStudentMarks.Text = "Please enter the student marks";
+            // 
+            // lblEnglish
+            // 
+            this.lblEnglish.AutoSize = true;
+            this.lblEnglish.Location = new System.Drawing.Point(48, 178);
+            this.lblEnglish.Name = "lblEnglish";
+            this.lblEnglish.Size = new System.Drawing.Size(41, 13);
+            this.lblEnglish.TabIndex = 6;
+            this.lblEnglish.Text = "English";
+            // 
+            // lblMaths
+            // 
+            this.lblMaths.AutoSize = true;
+            this.lblMaths.Location = new System.Drawing.Point(48, 203);
+            this.lblMaths.Name = "lblMaths";
+            this.lblMaths.Size = new System.Drawing.Size(36, 13);
+            this.lblMaths.TabIndex = 7;
+            this.lblMaths.Text = "Maths";
+            // 
+            // lblScience
+            // 
+            this.lblScience.AutoSize = true;
+            this.lblScience.Location = new System.Drawing.Point(48, 226);
+            this.lblScience.Name = "lblScience";
+            this.lblScience.Size = new System.Drawing.Size(46, 13);
+            this.lblScience.TabIndex = 8;
+            this.lblScience.Text = "Science";
+            // 
+            // gbxResults
+            // 
+            this.gbxResults.Controls.Add(this.lblStudentGrade);
+            this.gbxResults.Controls.Add(this.lblAverageMark);
+            this.gbxResults.Controls.Add(this.lblStudentDetails);
+            this.gbxResults.Location = new System.Drawing.Point(28, 266);
+            this.gbxResults.Name = "gbxResults";
+            this.gbxResults.Size = new System.Drawing.Size(431, 159);
+            this.gbxResults.TabIndex = 9;
+            this.gbxResults.TabStop = false;
+            this.gbxResults.Text = "Results";
+            // 
+            // lblStudentDetails
+            // 
+            this.lblStudentDetails.AutoSize = true;
+            this.lblStudentDetails.Location = new System.Drawing.Point(20, 43);
+            this.lblStudentDetails.Name = "lblStudentDetails";
+            this.lblStudentDetails.Size = new System.Drawing.Size(79, 13);
+            this.lblStudentDetails.TabIndex = 0;
+            this.lblStudentDetails.Text = "Student Details";
+            // 
+            // lblAverageMark
+            // 
+            this.lblAverageMark.AutoSize = true;
+            this.lblAverageMark.Location = new System.Drawing.Point(20, 79);
+            this.lblAverageMark.Name = "lblAverageMark";
+            this.lblAverageMark.Size = new System.Drawing.Size(74, 13);
+            this.lblAverageMark.TabIndex = 1;
+            this.lblAverageMark.Text = "Average Mark";
+            // 
+            // lblStudentGrade
+            // 
+            this.lblStudentGrade.AutoSize = true;
+            this.lblStudentGrade.Location = new System.Drawing.Point(23, 112);
+            this.lblStudentGrade.Name = "lblStudentGrade";
+            this.lblStudentGrade.Size = new System.Drawing.Size(76, 13);
+            this.lblStudentGrade.TabIndex = 2;
+            this.lblStudentGrade.Text = "Student Grade";
+            // 
+            // frmStudentGrades
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(563, 437);
+            this.Controls.Add(this.gbxResults);
+            this.Controls.Add(this.lblScience);
+            this.Controls.Add(this.lblMaths);
+            this.Controls.Add(this.lblEnglish);
+            this.Controls.Add(this.lblStudentMarks);
+            this.Controls.Add(this.lblGroup);
+            this.Controls.Add(this.lblStudentNumber);
+            this.Controls.Add(this.lblStudentName);
+            this.Controls.Add(this.lblEnterDetails);
+            this.Controls.Add(this.lblStudentGrades);
+            this.Name = "frmStudentGrades";
+            this.Text = "Student Grades";
+            this.gbxResults.ResumeLayout(false);
+            this.gbxResults.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblStudentGrades;
+        private System.Windows.Forms.Label lblEnterDetails;
+        private System.Windows.Forms.Label lblStudentName;
+        private System.Windows.Forms.Label lblStudentNumber;
+        private System.Windows.Forms.Label lblGroup;
+        private System.Windows.Forms.Label lblStudentMarks;
+        private System.Windows.Forms.Label lblEnglish;
+        private System.Windows.Forms.Label lblMaths;
+        private System.Windows.Forms.Label lblScience;
+        private System.Windows.Forms.GroupBox gbxResults;
+        private System.Windows.Forms.Label lblStudentGrade;
+        private System.Windows.Forms.Label lblAverageMark;
+        private System.Windows.Forms.Label lblStudentDetails;
+    }
+}
+
